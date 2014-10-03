9bfb61026356ecc258533e15a43c971d6ecdec59
jongarcia <jongarcia@wifi-roaming-128-4-219-231.host.udel.edu>
2014-10-02 23:09:43 -0400

    Add new check digit

---
+++ src/MainPackage/Test.java
@@ -0,0 +1,18 @@
+package MainPackage;
+
+import static org.junit.Assert.*;
+
+public class Test extends CheckDigit{
+
+
+	@org.junit.Test
+	public void Test() {
+		assertEquals("Valid for the given credit card number");
+	}
+
+	private void assertEquals(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
